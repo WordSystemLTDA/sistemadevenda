c26ed00d5b8d7802be163f7f33a13432ca648082
joaosiqueira <joaopedrochiquitin@gmail.com>
2025-01-08 12:58:27 -0300

    Update API connection URLs to use static version and remove package info dependency

--- lib/src/essencial/api/conexao.dart
+++ lib/src/essencial/api/conexao.dart
@@ -1,6 +1,5 @@
 import 'package:app/src/essencial/shared_prefs/chaves_sharedpreferences.dart';
 import 'package:app/src/essencial/shared_prefs/modelo_conexao.dart';
-import 'package:package_info_plus/package_info_plus.dart';
 
 class Apis {
   final ConfigSharedPreferences _config = ConfigSharedPreferences();
@@ -8,13 +7,10 @@ class Apis {
   Future<ModeloConexao> getConexao() async {
     var conexao = await _config.getConexao();
 
-    PackageInfo packageInfo = await PackageInfo.fromPlatform();
-    var versao = packageInfo.version;
-
     if (conexao!.tipoConexao == 'online') {
-      return ModeloConexao(tipoConexao: 'online', servidor: 'https://bigchef.com.br/sistema/apis_restaurantes/api_restaurantes_venda/$versao', porta: '');
+      return ModeloConexao(tipoConexao: 'online', servidor: 'https://bigchef.com.br/sistema/apis_restaurantes/api_restaurantes_venda/api1', porta: '');
     }
 
-    return ModeloConexao(tipoConexao: conexao.tipoConexao, porta: conexao.porta, servidor: 'http://${conexao.servidor}/sistema/apis_restaurantes/api_restaurantes_venda/1.0.01/');
+    return ModeloConexao(tipoConexao: conexao.tipoConexao, porta: conexao.porta, servidor: 'http://${conexao.servidor}/sistema/apis_restaurantes/api_restaurantes_venda/api1/');
   }
 }
